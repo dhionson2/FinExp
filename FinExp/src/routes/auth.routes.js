cc59316c3317cfaf94de954be5b250d5d056f0a5
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2023-07-31 22:15:13 -0300

    Cadastrando usuario no back, criação de contexto criação tela signin/up

--- FinExp/src/routes/auth.routes.js
+++ FinExp/src/routes/auth.routes.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import SingIn from '../pages/SingIn';
-import SingUp from '../pages/SingUp';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const AuthStack = createNativeStackNavigator();
 
 function AuthRoutes(){
-    return(
-        <AuthStack.Navigator>
-            <AuthStack.Screen 
-            name="Singin"
-            component={SingIn}/>
+  return(
+    <AuthStack.Navigator>
+      <AuthStack.Screen 
+        name="SignIn"
+        component={SignIn}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-
-        <AuthStack.Screen 
-            name="SingUp"
-            component={SingUp}/>    
-        </AuthStack.Navigator>
-    )
+      <AuthStack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          headerStyle:{
+            backgroundColor: '#3b3dbf',
+            borderBottomWidth: 1,
+            borderBottomColor: '#00b94a'
+          },
+          headerTintColor: '#FFF',
+          headerTitle: 'Voltar',
+          headerBackTitleVisible: false,
+        }}
+      />
+    </AuthStack.Navigator>
+  )
 }
 
 export default AuthRoutes;
--- FinExp/srcbkp/pages/Sing/index.js
+++ FinExp/srcbkp/pages/Sing/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function SingIn() {
+export default function Sing() {
  return (
    <View>
-        <Text>Tela login</Text>
+        <Text>Tela Sing</Text>
 
    </View>
   );
